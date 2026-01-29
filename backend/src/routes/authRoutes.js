ec2b9077b8c256bf92af6ebc9e95ba179297e169
HugoRepoMan <hudarmijosq@gmail.com>
2026-01-28 19:48:18 -0500

    Mejoras en seguridad y BD 3

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-
+//Rutas
 router.post('/registro', authController.registrarUsuario);
 router.post('/login', authController.login);
 
